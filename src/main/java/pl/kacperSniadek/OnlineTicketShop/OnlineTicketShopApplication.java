deef8885928c15face690780e24324f605df60e1
Kacper Śniadek <k.cs.kesz@gmail.com>
2023-11-19 19:53:25 +0100

    first commit

---
+++ src/main/java/pl/kacperSniadek/OnlineTicketShop/OnlineTicketShopApplication.java
@@ -0,0 +1,13 @@
+package pl.kacperSniadek.OnlineTicketShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTicketShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTicketShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kacperSniadek/OnlineTicketShop/OnlineTicketShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kacperSniadek.OnlineTicketShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTicketShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
